1462513304614512e96e1ea8786b10355bf89a1f
bakkaSansanBakka <polina.potapeyko@mail.ru>
2022-07-14 19:38:34 +0300

    DbContext for models created, migration performed and Program.cs modified to use created DbContext

---
+++ ComicsWebApp/Data/ComicsDbContext.cs
@@ -0,0 +1,40 @@
+﻿using ComicsWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ComicsWebApp.Data
+{
+    public class ComicsDbContext: DbContext
+    {
+        public DbSet<Comics> Comics { get; set; }
+        public DbSet<ComicsGenre> ComicsGenres { get; set; }
+
+        public ComicsDbContext(DbContextOptions<ComicsDbContext> options)
+            : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Comics>()
+                .Property(c => c.Language)
+                .HasConversion<string>();
+
+            modelBuilder.Entity<Comics>()
+                .Property(c => c.AvailabilityStatus)
+                .HasConversion<string>();
+
+            modelBuilder.Entity<ComicsGenre>().HasData(
+                new ComicsGenre[]
+                {
+                    new ComicsGenre {Id = 1, GenreName = "Detective"},
+                    new ComicsGenre {Id = 2, GenreName = "Historical"},
+                    new ComicsGenre {Id = 3, GenreName = "Science Fiction"},
+                    new ComicsGenre {Id = 4, GenreName = "Educational"},
+                    new ComicsGenre {Id = 5, GenreName = "Adventure"},
+                    new ComicsGenre {Id = 6, GenreName = "Romantic"},
+                    new ComicsGenre {Id = 7, GenreName = "Horror"},
+                    new ComicsGenre {Id = 8, GenreName = "Fantasy"},
+                    new ComicsGenre {Id = 9, GenreName = "Humor"}
+                });
+        }
+    }
+}
--- ComicsWebApp/Program.cs
+++ ComicsWebApp/Program.cs
@@ -8,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+builder.Services.AddDbContext<ComicsDbContext>(options =>
+    options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
