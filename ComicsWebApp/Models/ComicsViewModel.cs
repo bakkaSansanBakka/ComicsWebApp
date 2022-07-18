e4a40a3634c7ac7cf21bf8656161df556e490517
bakkaSansanBakka <polina.potapeyko@mail.ru>
2022-07-18 17:49:17 +0300

    created view model for Add Comics view

---
+++ ComicsWebApp/Models/ComicsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace ComicsWebApp.Models
+{
+    public class ComicsViewModel
+    {
+        public Comics Comics { get; set; }
+        public List<SelectListItem> Genres { get; set; }
+        public int[] GenresIds { get; set; }
+    }
+}
