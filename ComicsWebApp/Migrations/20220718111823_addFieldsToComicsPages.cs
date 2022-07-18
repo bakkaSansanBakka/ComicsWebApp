f218daecbd120560cfc4fd9502cfbc7fd5dd5086
bakkaSansanBakka <polina.potapeyko@mail.ru>
2022-07-18 17:40:05 +0300

    performed migrations to Database

---
+++ ComicsWebApp/Migrations/20220718111823_addFieldsToComicsPages.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ComicsWebApp.Migrations
+{
+    public partial class addFieldsToComicsPages : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FileType",
+                table: "ComicsPages",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "ComicsPages",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FileType",
+                table: "ComicsPages");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "ComicsPages");
+        }
+    }
+}
