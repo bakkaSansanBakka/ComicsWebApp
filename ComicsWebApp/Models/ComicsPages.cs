8c7b6094972a055518578e2c37ade72563c41c2d
bakkaSansanBakka <polina.potapeyko@mail.ru>
2022-07-18 17:36:53 +0300

    added Name and FileType properties to ComicsPages model

--- ComicsWebApp/Models/ComicsPages.cs
+++ ComicsWebApp/Models/ComicsPages.cs
@@ -3,7 +3,9 @@
     public class ComicsPages
     {
         public int Id { get; set; }
+        public string Name { get; set; }
         public byte[] Content { get; set; }
+        public string FileType { get; set; }
 
         public int ComicsId { get; set; }
         public Comics Comics { get; set; }
