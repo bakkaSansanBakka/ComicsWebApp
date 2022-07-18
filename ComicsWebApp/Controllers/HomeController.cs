575cbe4d021ecaeb9ce11638d4fc5e721d2a313e
bakkaSansanBakka <polina.potapeyko@mail.ru>
2022-07-18 17:51:48 +0300

    created actions for Add Comics view

--- ComicsWebApp/Controllers/HomeController.cs
+++ ComicsWebApp/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using ComicsWebApp.Models;
+﻿using ComicsWebApp.Data;
+using ComicsWebApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Diagnostics;
 
 namespace ComicsWebApp.Controllers
@@ -7,10 +9,12 @@ namespace ComicsWebApp.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ComicsDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ComicsDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -21,6 +25,55 @@ namespace ComicsWebApp.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public IActionResult AddComics()
+        {
+            var comicsViewModel = new ComicsViewModel();
+            comicsViewModel.Genres = _context.ComicsGenres.Select(g => new SelectListItem { Text = g.GenreName, Value = g.Id.ToString() }).ToList();
+            return View(comicsViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult AddComics(ComicsViewModel comicsViewModel, IFormFile CoverFile)
+        {
+            ComicsGenre comicsGenre = new ComicsGenre();
+
+            Comics comics = new Comics();
+            comics.Name = comicsViewModel.Comics.Name;
+            comics.Author = comicsViewModel.Comics.Author;
+            comics.Price = comicsViewModel.Comics.Price;
+            comics.CoverType = comicsViewModel.Comics.CoverType;
+            comics.Language = comicsViewModel.Comics.Language;
+            comics.Publisher = comicsViewModel.Comics.Publisher;
+            comics.AvailabilityStatus = comicsViewModel.Comics.AvailabilityStatus;
+            comics.PagesNumber = comicsViewModel.Comics.PagesNumber;
+            comics.PublicationFormat = comicsViewModel.Comics.PublicationFormat;
+            comics.YearOfPublisihing = comicsViewModel.Comics.YearOfPublisihing;
+            comics.Description = comicsViewModel.Comics.Description;
+
+            if (comicsViewModel.GenresIds.Length > 0)
+            {
+                foreach (var genreid in comicsViewModel.GenresIds)
+                {
+                    comicsGenre = _context.ComicsGenres.FirstOrDefault(g => g.Id == genreid);
+                    comics.Genres.Add(comicsGenre);
+                }
+            }
+
+            if (CoverFile.Length > 0)
+            {
+                using (var target = new MemoryStream())
+                {
+                    CoverFile.CopyTo(target);
+                    comics.Cover = target.ToArray();
+                }
+            }
+
+            _context.Comics.Add(comics);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
