ca4b9ea18be958b866f89740bdd9492789c13c24
bakkaSansanBakka <polina.potapeyko@mail.ru>
2022-07-27 19:03:09 +0300

    Rename Genres property to AllGenresList

--- ComicsWebApp/Models/ComicsViewModel.cs
+++ ComicsWebApp/Models/ComicsViewModel.cs
@@ -5,7 +5,7 @@ namespace ComicsWebApp.Models
     public class ComicsViewModel
     {
         public Comics Comics { get; set; }
-        public List<SelectListItem> Genres { get; set; }
+        public List<SelectListItem> AllGenresList { get; set; }
         public List<ComicsGenre> ListOfGenres { get; set; }
         public List<ComicsPages> ListOfPages { get; set; }
         public int[] GenresIds { get; set; }
