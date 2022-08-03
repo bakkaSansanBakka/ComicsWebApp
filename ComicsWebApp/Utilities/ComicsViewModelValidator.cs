0aeff974a26cdce38926f902ae2c7457142d7e63
bakkaSansanBakka <polina.potapeyko@mail.ru>
2022-08-03 16:41:25 +0300

    Add validation messages for number fields

--- ComicsWebApp/Utilities/ComicsViewModelValidator.cs
+++ ComicsWebApp/Utilities/ComicsViewModelValidator.cs
@@ -9,17 +9,15 @@ namespace ComicsWebApp.Utilities
         {
             RuleFor(c => c.Comics.Name).NotEmpty();
             RuleFor(c => c.Comics.Author).NotEmpty();
-            RuleFor(c => c.Comics.Price).GreaterThan(0);
+            RuleFor(c => c.Comics.Price).GreaterThan(0).WithMessage("Price should be greater than 0.");
             RuleFor(c => c.Comics.Cover).NotNull();
             RuleFor(c => c.Comics.CoverType).IsInEnum();
             RuleFor(c => c.Comics.Language).IsInEnum();
             RuleFor(c => c.Comics.Publisher).NotEmpty();
             RuleFor(c => c.Comics.AvailabilityStatus).IsInEnum();
-            RuleFor(c => c.Comics.PagesNumber).GreaterThan(0);
+            RuleFor(c => c.Comics.PagesNumber).GreaterThan(0).WithMessage("Number of pages should be greater than 0.");
             RuleFor(c => c.Comics.PublicationFormat).NotEmpty();
-            RuleFor(c => c.Comics.YearOfPublisihing).NotEmpty();
-
-            RuleFor(c => c.GenresIds).NotNull();
+            RuleFor(c => c.Comics.YearOfPublisihing).GreaterThan(0).WithMessage("Year of publication should be greater than 0.");
         }
     }
 }
