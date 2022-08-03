bb5597246d4eb6917d4fffe0cf92ec5f5df994cb
bakkaSansanBakka <polina.potapeyko@mail.ru>
2022-08-03 18:51:54 +0300

    Move all enums to separate file

--- ComicsWebApp/Data/Models/Comics.cs
+++ ComicsWebApp/Data/Models/Comics.cs
@@ -1,36 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace ComicsWebApp.Data.Models
+﻿namespace ComicsWebApp.Data.Models
 {
-
-    public enum ComicsLanguage
-    {
-        English,
-        French,
-        Russian,
-        Spanish,
-        Italian,
-        Chinese,
-        Japanese,
-        Deutch
-    }
-
-    public enum ComicsAvailabilityStatus
-    {
-        [Display(Name = "In Stock")]
-        InStock,
-        [Display(Name = "Sold Out")]
-        SoldOut,
-        [Display(Name = "On Order")]
-        OnOrder
-    }
-
-    public enum CoverType
-    {
-        Hard,
-        Soft
-    }
-
     public class Comics
     {
         public int Id { get; set; }
